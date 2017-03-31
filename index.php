7e327c69a8e58c39843ea4c07aae96a245565e71
Mujtaba <mujtaba.ahmad91@gmail.com>
2017-03-31 11:28:59 +0000

    Setting up stuff

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-hello
+<?php 
+
+
+$requestUri = substr($_SERVER["REQUEST_URI"], 1);
+
+if($requestUri === "") {
+
+}elseif(file_exists("ahmadmujtaba.me/".$requestUri)) {
+    include($requestUri);
+} else {
+    include("HTTP404.html");
+}
+
+
+?>
