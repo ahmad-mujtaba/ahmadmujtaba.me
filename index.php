91801df2636347b1b59df0a3e339f5544b0fc59d
Mujtaba <mujtaba.ahmad91@gmail.com>
2017-04-01 00:56:05 +0530

    Added index.html

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $requestUri = substr($_SERVER["REQUEST_URI"], 1);
 
 if($requestUri === "") {
 
-}elseif(file_exists("ahmadmujtaba.me/".$requestUri)) {
+include("index.html");}elseif(file_exists("ahmadmujtaba.me/".$requestUri)) {
     include($requestUri);
 } else {
     include("HTTP404.html");
