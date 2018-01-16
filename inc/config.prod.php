9fe21bc6f3e74ea7731aafa58e25c62acda642d9
Mujtaba Ahmad <mujtaba.ahmad91@gmail.com>
2018-01-16 18:59:38 +0530

    Update config.prod.php

--- inc/config.prod.php
+++ inc/config.prod.php
@@ -1,6 +1,6 @@
 <?php
 
 	define('SITE_ROOT', "//ahmadmujtaba.me/ahmadmujtaba.me");
-	$staticAssetsVersion = "?v=4";	// bump this everytime significant changes are made
+	$staticAssetsVersion = "?v=5";	// bump this everytime significant changes are made
 	
 ?>
